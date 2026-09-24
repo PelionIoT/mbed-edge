8c18f86c657a2c9dc6c7cc0406ab08b6ffa4834b
Ed Hemphill <edhemphill@gmail.com>
2026-09-24 01:59:44 -0500

    Integrate native Windows PAL libraries

--- config/pal_windows.h
+++ config/pal_windows.h
@@ -8,5 +8,10 @@
 #define PAL_NET_MAX_IF_NAME_LENGTH 256
 #define PAL_NET_DNS_SUPPORT 1
 #define PAL_USE_FILESYSTEM 1
+#ifndef PAL_DNS_API_VERSION
+/* Use PAL's existing asynchronous DNS worker around the platform resolver. */
+#define PAL_DNS_API_VERSION 1
+#endif
+#define PAL_NET_ASYNC_DNS_THREAD_STACK_SIZE (128 * 1024)
 
 #endif
